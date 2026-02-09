fa1a78e2922f955fe3b4121245f2d500e4fa63f9
Punpichai Seesoda <39486576+lnwchai@users.noreply.github.com>
2026-02-09 14:39:04 +0700

    Update single-nida_impact.php

--- single-nida_impact.php
+++ single-nida_impact.php
@@ -24,13 +24,32 @@ get_header();
 
 
             
-            if (get_field('research_download_url')) {
+            if (class_exists('GFAPI')) {
+
+                $form_id   = 4;
+                $page_name = get_the_title();
+            
+                $search_criteria = array(
+                    'status' => 'active',
+                    'field_filters' => array(
+                        array(
+                            'key'   => '8',              // Field ID 8
+                            'value' => $page_name        // ชื่อหน้า
+                        )
+                    )
+                );
+            
+                $count = GFAPI::count_entries($form_id, $search_criteria);
+            }
+            
+            if (get_field('impack_download_url')) {
 
                 $download_url = get_field('impack_download_url');
             
                 echo '<div class="text-center">';
                 echo '<a href="' . esc_url(add_query_arg('download', base64_encode($download_url))) . '" class="btn btn-outline">';
-                echo 'Download full paper';
+                echo 'Download full paper <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-down-icon lucide-file-down"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M12 18v-6"/><path d="m9 15 3 3 3-3"/></svg>'. number_format($count);
+             
                 echo '</a>';
                 echo '</div>';
             }
@@ -64,7 +83,7 @@ get_header();
             echo '<div class="sdg-view">';
           
             
-            echo do_shortcode('[post-views]');
+            echo do_shortcode('[s_stat]');
             
             echo '</div>';
 
